075ba884dedc63bbf7596e36cc80b395e99a8d6b
namca <114391153+20092004nam2k4@users.noreply.github.com>
2024-07-10 15:25:21 +0700

    main

---
+++ src/main/java/com/example/shoppet/controller/PetController.java
@@ -0,0 +1,9 @@
+package com.example.shoppet.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller("Pet")
+public class PetController {
+}
